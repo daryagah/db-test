09a9f135025876e94588487c2852185aff9647ee
Darya Gahramanova <59887251+daryagah@users.noreply.github.com>
2020-07-01 19:48:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ server.listen(PORT);
 console.log(`Server is running on port ${PORT}`);
 
 const { Client } = require('pg');
-const connectionString = process.env.DATABASE_URL;
 const client = new Client({
     user: 'postgres',
     host: 'cloudsql-proxy',
